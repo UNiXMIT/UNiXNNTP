72d4ee307b740dad0121f4fc26d735dad7fb2dc7
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-12-01 18:46:15 +0000

    1.1.1

--- build.cmd
+++ build.cmd
@@ -3,17 +3,16 @@
 SET OPTS=%1
 
 FOR /F %%V IN ('jq -r ".version" "manifest.json"') DO SET "VER=%%V"
-SET NNTPNotify=NNTPNotify-%VER%.zip
 
 IF "%OPTS%"=="submit" GOTO :SUBMIT
 
 :BUILD
-CALL web-ext build --overwrite-dest --filename NNTPNotify-%VER%.zip --artifacts-dir . --source-dir .\ --ignore-files FFAPI.json *.cmd *.md updates/
+CALL web-ext build --overwrite-dest --filename NNTPNotify-%VER%.zip --artifacts-dir . --source-dir ./ --ignore-files package.json *.cmd *.md updates/
 MKDIR updates\%VER%
 GOTO :END
 
 :SUBMIT
-CALL web-ext sign --source-dir ./SFExtFF/ --channel unlisted
+CALL web-ext sign --source-dir ./ --ignore-files package.json *.cmd *.md updates/ --channel unlisted
 GOTO :END
 
 :END
